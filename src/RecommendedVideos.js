ae308990680b825ca45f2a4b55e759ab38f7425c
47720480 <david.n.stahl@gmail.com>
2020-10-07 18:03:52 +0200

    header done and working on sidebar

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./RecommendedVideos.css"
+
+function RecommendedVideos() {
+    return (
+        <div className="recommendedVideos">
+            this is where videos go
+        </div>
+    )
+}
+
+export default RecommendedVideos
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC319x5acab0qSrJqP96OkO5syK18lYRM8",
+    authDomain: "clone-39a0f.firebaseapp.com",
+    databaseURL: "https://clone-39a0f.firebaseio.com",
+    projectId: "clone-39a0f",
+    storageBucket: "clone-39a0f.appspot.com",
+    messagingSenderId: "974725589244",
+    appId: "1:974725589244:web:c800988829492548980f79"
+  };
